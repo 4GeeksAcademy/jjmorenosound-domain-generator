ae023b77df46b2882752fbe8d827510b793e867f
Jorge Moreno <jjmorenoaudio@gmail.com>
2024-10-18 18:41:24 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,41 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
+
+  //write your code here
+  let pronoun = ["the", "our", "your", "my", "this"];
+  let adj = ["great", "big", "amazing", "spectacular", "friendly"];
+  let noun = ["jogger", "racoon", "spider", "man", "rocket"];
+  let donknow = ["in", "from", "outof", "within", "outsideof"];
+  let place = ["sky", "hell", "mountain", "paris", "underthesea"];
+  let extension = [".com", ".net", ".io", ".es", ".org"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < donknow.length; l++) {
+          for (let m = 0; m < place.length; m++) {
+            for (let n = 0; n < extension.length; n++) {
+              console.log(
+                `${pronoun[i]}${adj[j]}${noun[k]}${donknow[l]}${place[m]}${extension[n]}`
+              );
+            }
+          }
+        }
+      }
+    }
+  }
 };
+
+// const matrixBuilder = (num) => {
+//   let matrix = []
+//   for (let i = 0; i < num; i++) {
+//       matrix.push([])
+//       for (let j = 0; j < num; j++) {
+//           matrix[i].push(getRandom())
+//       }
+
+//   }
+//   return matrix;
+// }
